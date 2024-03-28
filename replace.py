e16d280e554310559d07bf589b4b0e3e7e177d1c
NyxIsBad <hijirijst@gmail.com>
2024-03-28 08:56:54 -0400

    update replace.py to work with directories.

--- replace.py
+++ replace.py
@@ -1,19 +1,24 @@
-# regex and csv handling
-import csv
-import re
 # pandas for csv handling
 import pandas as pd
 # fileinput for find and replace
 import fileinput
 # cl arguments
 import argparse
-import time
+import os
+import time 
 
 delimiter = ','
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--classes_mapping', default='classes_mapping.csv', help='Input classes mapping from old to new. Default: classes_mapping.csv')
-parser.add_argument('--replace_input', default='../lib/selectors/selectorPlaceholders.scss', help='Input filename. Default: ../lib/selectors/selectorPlaceholders.scss')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f','--file', help='Input filename path.')
+group.add_argument('-d','--directory', help='Input directory.')
+
+args = parser.parse_args()
+csv_output = args.classes_mapping
+file_in = args.file if args.file != '' else None
+dir_in = args.directory if args.directory != '' else None
 
 def load_csv(input):
     data = pd.read_csv(input, sep=delimiter, header=0, keep_default_na=False)
@@ -26,7 +31,7 @@ def find_and_replace(csv_output, replace_input):
     df = df[(df['class_old'] != '$NOTFOUND$') & (df['class_new'] != '$NOTFOUND$')]
     replace_dict = dict(zip(df['class_old'], df['class_new'])).items()
     # we're just gonna brute force it
-    with fileinput.FileInput(replace_input, inplace=True, backup=".bak") as file:
+    with fileinput.FileInput(replace_input, inplace=True) as file:
         for line in file:
             for class_old, class_new in replace_dict:
                 line = line.replace(class_old, class_new)
@@ -34,7 +39,14 @@ def find_and_replace(csv_output, replace_input):
             print(line, end='')
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    csv_output = args.classes_mapping
-    replace_input = args.replace_input
-    find_and_replace(csv_output, replace_input)
+    start_time = time.time()
+    print(file_in, dir_in)
+    if file_in:
+        find_and_replace(csv_output, file_in)
+    elif dir_in:
+        for subdir, _, files in os.walk(dir_in):
+            for file in files:
+                file_path = os.path.join(subdir, file)
+                print("Processing: ", file_path)
+                find_and_replace(csv_output, file_path)
+    print(f'--- {time.time() - start_time} seconds ---')
