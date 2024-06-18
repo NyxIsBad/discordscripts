1f5a82ce755f6141e6daa233977f53dffb318ae4
NyxIsBad <hijirijst@gmail.com>
2024-06-18 19:16:18 -0400

    in which @XYZenix carries me

---
+++ 11pixels.py
@@ -0,0 +1,26 @@
+# regex and csv handling
+import csv
+import re
+# pandas for csv handling
+import pandas as pd
+# fileinput for find and replace
+import fileinput
+# cl arguments
+import argparse
+import time
+
+def parse_diff():
+    with open("11pixels.txt",'r') as fin, open("11pixels.csv",'w') as fcsvout, open("11pixels.diff",'w') as fdiffout:
+        while True:
+            line1 = fin.readline().strip()
+            line2 = fin.readline().strip()
+
+            if not line1 and not line2:
+                break
+
+            if line1 != "__MISSINGNO__" and line2 != "__MISSINGNO__":
+                fcsvout.write(f"{line1},{line2}\n")
+                fdiffout.write(f"-{line1}\n+{line2}\n")
+        
+
+parse_diff()
